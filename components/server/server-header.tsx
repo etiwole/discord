9ed91fe7efa7b410ff1bebb3359aa77ca67756c9
Андрей Коногоров <a.konogorov@uniqom.ru>
2023-09-03 15:11:19 +0700

    channel creation has been added

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -66,6 +66,7 @@ const ServerHeader: FC<ServerHeaderProps> = ({ server, role }) => {
           )}
           {isModerator && (
             <DropdownMenuItem
+              onClick={() => onOpen('createChannel', { server })}
               className="px-3 py-2 text-sm cursor-pointer"
             >
               Create Channel
