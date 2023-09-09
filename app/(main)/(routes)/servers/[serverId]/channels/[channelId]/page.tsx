372eb33401b7a378f609a5cf8b0088a9b050df5b
Андрей Коногоров <a.konogorov@uniqom.ru>
2023-09-09 15:18:02 +0700

    chat header has been added

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {currentProfile} from "@/lib/current-profile";
+import {redirectToSignIn} from "@clerk/nextjs";
+import {db} from "@/lib/db";
+import {redirect} from "next/navigation";
+import ChatHeader from "@/components/chat/chat-header";
 
-const ChannelIdPage = () => {
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  }
+}
+
+const ChannelIdPage: FC<ChannelIdPageProps> = async ({ params }) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile?.id,
+    }
+  });
+
+  if (!channel || !member) {
+    redirect('/')
+  }
+
   return (
-    <div>
-      Channel ID Page!
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
     </div>
   );
 };
