7ac50a56b894c28251a6fd9688b1771104377eeb
Андрей Коногоров <a.konogorov@uniqom.ru>
2023-11-05 12:01:30 +0700

    up

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,7 @@ import {redirectToSignIn} from "@clerk/nextjs";
 import {db} from "@/lib/db";
 import {redirect} from "next/navigation";
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
   params: {
@@ -39,6 +40,16 @@ const ChannelIdPage: FC<ChannelIdPageProps> = async ({ params }) => {
   return (
     <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader name={channel.name} serverId={channel.serverId} type="channel"/>
+      <div className="flex-1">Future messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelOd: channel.id,
+          serverId: channel.serverId
+        }}
+      />
     </div>
   );
 };
